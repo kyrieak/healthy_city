b6ccf14c30d3f7d9014307a75d1e75ddf277c129
Kyrie Kopczynski <kyrie.ak@gmail.com>
2013-01-13 23:14:14 -0800

    removing some deleted files from changes not staged for commit

--- app/helpers/icon_sets_helper.rb
+++
@@ -1,2 +0,0 @@
-module IconSetsHelper
-end
--- app/models/icon_set.rb
+++
@@ -1,6 +0,0 @@
-class IconSet < ActiveRecord::Base
-  attr_accessible :user_id, :icon_id, :selected, :action_name
-
-  belongs_to :user
-  has_many :icons
-end
